79186479f072a153af7fc43fa776a7fc7a9d879a
eduardomingoranca <eduardomingoranca@gmail.com>
2021-07-17 10:36:17 -0300

    resttemplate exchange

--- src/main/java/study/project/essentials/client/SpringClient.java
+++ src/main/java/study/project/essentials/client/SpringClient.java
@@ -1,9 +1,14 @@
 package study.project.essentials.client;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import study.project.essentials.domain.Anime;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
@@ -14,5 +19,19 @@ public class SpringClient {
         Anime object =new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class,1);
 
         log.info(object);
+
+        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
+
+        log.info(Arrays.toString(animes));
+
+        // convertendo array para lista
+        //@formatter:off
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {});
+
+        // @formatter:on
+        log.info(exchange.getBody());
     }
 }
